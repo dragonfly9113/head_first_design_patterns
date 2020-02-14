7440fa524aba85c5a2911216c067e5ce7d083256
William Luo <weilin.william.luo@gmail.com>
2020-02-14 12:03:28 -0800

    Added Duck related classes and interfaces and MiniDuckSimulator.

---
+++ HelloWorld/src/MallardDuck.java
@@ -0,0 +1,13 @@
+import java.util.Queue;
+
+public class MallardDuck extends Duck {
+
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
+    public void display() {
+        System.out.println("I'm a real Mallard duck.");
+    }
+}
