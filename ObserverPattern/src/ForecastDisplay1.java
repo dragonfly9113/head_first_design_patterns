3ce62e3fa1c26082821e11963184299ef399a422
William Luo <weilin.william.luo@gmail.com>
2020-02-16 22:19:08 -0800

    Use Obserable and Observer classes

--- ObserverPattern/src/ForecastDisplay1.java
+++ ObserverPattern/src/ForecastDisplay1.java
@@ -7,7 +7,27 @@ public class ForecastDisplay1 implements Observer, DisplayElement {
     private Observable observable;
 
     public ForecastDisplay1(Observable observable) {
-
+        this.observable = observable;
+        observable.addObserver(this);
     }
 
+    public void update(Observable observable, Object arg) {
+        if (observable instanceof WeatherData1) {
+            WeatherData1 weatherData = (WeatherData1) observable;
+            lastPressure = currentPressure;
+            currentPressure = weatherData.getPressure();
+            display();
+        }
+    }
+
+    public void display() {
+        System.out.print("Forecast: ");
+        if (currentPressure > lastPressure) {
+            System.out.println("Improving weather on the way!");
+        } else if (currentPressure == lastPressure) {
+            System.out.println("More of the same");
+        } else if (currentPressure < lastPressure) {
+            System.out.println("Watch out for the cooler, rainy weather");
+        }
+    }
 }
--- ObserverPattern/src/WeatherStation.java
+++ ObserverPattern/src/WeatherStation.java
@@ -1,6 +1,6 @@
 public class WeatherStation {
 
-    public static void main(String[] args) {
+    public static void main_old(String[] args) {
 	    WeatherData weatherData = new WeatherData();
 
 	    CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
@@ -12,4 +12,18 @@ public class WeatherStation {
 	    weatherData.setMeasurements(82, 70, 29.2f);
 	    weatherData.setMeasurements(78, 90, 29.2f);
     }
+
+	public static void main(String[] args) {
+		WeatherData1 weatherData = new WeatherData1();
+
+		CurrentConditionsDisplay1 currentDisplay = new CurrentConditionsDisplay1(weatherData);
+		//StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+		ForecastDisplay1 forecastDisplay = new ForecastDisplay1(weatherData);
+		//HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+
+		weatherData.setMeasurements(80, 65, 30.4f);
+		weatherData.setMeasurements(82, 70, 29.2f);
+		weatherData.setMeasurements(78, 90, 29.2f);
+	}
+
 }
